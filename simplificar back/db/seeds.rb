287b6152ac536d865126dbbfba45d51ce21a1413
github username <github email address>
2017-12-07 09:39:20 -0500

    ostly css and untracked

--- simplificar back/db/seeds.rb
+++ simplificar back/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+cory = User.create(name: "Cory")
+fabiano = User.create(name: "Fabiano")
+mickey = User.create(name: "Mickey Mouse")
+
+temp1 = Template.new(name: "temp1")
+temp2 = Template.new(name: "temp2")
+temp3 = Template.new(name: "temp3")
+
+cory.templates << temp1
+cory.templates << temp2
+fabiano.templates << temp3
